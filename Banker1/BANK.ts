aea869122a726fbf4e8f7b8efafa0eae1aa6f5ed
Krishnasolanki5383 <ks9522769@gmail.com>
2026-09-25 09:49:49 +0530

    Created bank Assignment

---
+++ Banker1/BANK.ts
@@ -0,0 +1,53 @@
+class bank
+{
+  static totalbankBalance:number=0;
+
+  id:number;
+  name:string;
+  amount:number;
+
+  constructor(id:number,name:string,amount:number)
+  {
+    this.id=id;
+    this.name=name;
+    this.amount=amount;
+    bank.totalbankBalance+=amount;
+  };
+   debit(am:number):void{
+    if (am > this.amount) {
+    console.log("Insufficient Balance");
+    return;
+}
+    this.amount-=am;
+    bank.totalbankBalance-=am;
+    
+    console.log(`your account debit: ${am} amount`)
+  }
+
+   credit(am:number):void{
+    this.amount+=am;
+    bank.totalbankBalance+=am;
+     console.log(`your account credit: ${am} amount`)
+  }
+
+  accountInfo():void{
+ console.log(`
+Account ID : ${this.id}
+Name : ${this.name}
+Balance : ₹${this.amount}
+`);
+  }
+
+}
+
+const u1=new bank(21,"krishna",21210);
+const u2=new bank(212,"chotu",1000);
+const u3=new bank(27,"jal",50010);
+u1.accountInfo()
+
+console.log(`totla amount in bank is ${bank.totalbankBalance}`);
+
+u1.credit(3000)
+console.log(`totla amount in bank is ${bank.totalbankBalance}`);
+
+
